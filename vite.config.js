989432754e9fe19be879a8420a09561e6efbb664
J. David Ibáñez <jdavid.ibp@gmail.com>
2025-02-28 18:17:38 +0100

    vite: support multiple entry points

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-    let entry = 'src/main.js';
+    const admin = resolve(__dirname, 'src/admin.js')
+    const front = resolve(__dirname, 'src/front.js')
+
     let config = {
         base: '/static/',
         build: {
+            cssCodeSplit: true,
             manifest: "manifest.json",
             outDir: 'var/build',
             rollupOptions: {
@@ -17,18 +21,22 @@ export default defineConfig(({ command, mode }) => {
 
     if (command === 'serve') {
         config.build.rollupOptions = {
-            input: {
-                main: entry,
-            }
+            input: {admin}
         }
-    } else { // build
+    }
+    else { // build
         config.build.lib = {
-            entry,
+            entry: {admin},
             formats: ['es'],
-            fileName: (format) => 'main.js',
+            fileName: (format, entryName) => `${entryName}.js`,
+        }
+        // In lib mode filenames don't include a hash by default, add one here
+        config.build.rollupOptions.output = {
+            entryFileNames: '[name]-[hash].js',
+            assetFileNames: '[name]-[hash][extname]',
         }
     }
 
-    //console.log('CONFIG', config);
-    return config;
+    //console.log('CONFIG', config)
+    return config
 })
