551291014540e6a4ac06b21ee49ee40fab3ba550
J. David Ibáñez <jdavid.ibp@gmail.com>
2025-03-17 18:48:28 +0100

    Fix ping

--- mods/ping/urls.py
+++ mods/ping/urls.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_not_required
 from django.http import HttpResponse
 from django.urls import path
 
 
+@login_not_required
 def ping(request):
     return HttpResponse('pong', content_type='text/plain')
 
